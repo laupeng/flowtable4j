35b36f9b64fb8f5fd282c2d17f0f2ea290983dce
thyang <thyang@ctrip.com>
2015-03-12 18:32:07 +0800

    init

---
+++ flowtable4j-core/src/main/java/com/ctrip/flowtable4j/core/Utils/CommonUtils.java
@@ -0,0 +1,182 @@
+package com.ctrip.flowtable4j.core.Utils;
+
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ctrip.infosec.sars.util.GlobalConfig;
+
+/**
+ * 
+ * @author weiyu
+ * @date 2015年3月12日
+ */
+public class CommonUtils {
+	@Autowired
+	private Properties appProperties;
+
+	public String getAppId() {
+		return appProperties.getProperty("APPID");
+	}
+
+	static String eSBUrl = null;
+
+	public String getESBUrl() {
+		if (eSBUrl == null || eSBUrl.isEmpty()) {
+			eSBUrl = appProperties.getProperty("ESBUrl");
+		}
+		return eSBUrl;
+	}
+
+	/**
+	 * redis 
+	 * @return
+	 */
+	public String getCRedisServiceUrl() {
+		return GlobalConfig.getString("CRedis.serviceUrl");
+	}
+	
+	public boolean getCRedisLogging(){
+		if("true".equalsIgnoreCase(GlobalConfig.getString("CRedis.logging"))){
+			return true;
+		}
+		else
+			return false;
+	}
+
+	/**
+	 * clogging
+	 * @return
+	 */
+	public String getLoggingServerIP() {
+		return GlobalConfig.getString("LoggingServerIP");
+	}
+
+	public String getLoggingServerPort() {
+		return GlobalConfig.getString("LoggingServerPort");
+	}
+
+
+
+
+
+	static String hostIP = null;
+
+	public String getHostIP() {
+		if (hostIP == null || hostIP.isEmpty()) {
+			hostIP = getLocalIP();
+		}
+		return hostIP;
+	}
+
+	public String getProxyHost() {
+		String proxyHost = "";
+		try {
+			proxyHost = appProperties.getProperty("ProxyHost");
+		} catch (Exception e) {
+
+		}
+
+		return proxyHost;
+	}
+
+	public int getProxyPort() {
+		int ProxyPort = 8080;
+		try {
+			String t = appProperties.getProperty("ProxyPort");
+			ProxyPort = org.apache.commons.lang.math.NumberUtils.toInt(t, 8080);
+		} catch (Exception e) {
+
+		}
+
+		return ProxyPort;
+	}
+
+	/**
+	 * 
+	 * 判斷當前操作是否Windows.
+	 * 
+	 * 
+	 * 
+	 * @return true---是Windows操作系統
+	 */
+
+	public static boolean isWindowsOS() {
+
+		boolean isWindowsOS = false;
+		String osName = System.getProperty("os.name");
+		if (osName.toLowerCase().indexOf("windows") > -1) {
+			isWindowsOS = true;
+		}
+		return isWindowsOS;
+	}
+
+	/**
+	 * 
+	 * 獲取本機IP地址，並自動區分Windows還是Linux操作系統
+	 * 
+	 * 
+	 * 
+	 * @return String
+	 */
+
+	private static String getLocalIP() {
+		String sIP = "";
+		InetAddress ip = null;
+		try {
+			// 如果是Windows操作系統
+			if (isWindowsOS()) {
+				ip = InetAddress.getLocalHost();
+			}
+			// 如果是Linux操作系統
+			else {
+				boolean bFindIP = false;
+				Enumeration<NetworkInterface> netInterfaces = (Enumeration<NetworkInterface>) NetworkInterface.getNetworkInterfaces();
+
+				while (netInterfaces.hasMoreElements()) {
+					if (bFindIP) {
+						break;
+					}
+
+					NetworkInterface ni = (NetworkInterface) netInterfaces.nextElement();
+
+					// ----------特定情況，可以考慮用ni.getName判斷
+					// 遍歷所有ip
+					Enumeration<InetAddress> ips = ni.getInetAddresses();
+					while (ips.hasMoreElements()) {
+						ip = (InetAddress) ips.nextElement();
+						if (ip.isSiteLocalAddress() && !ip.isLoopbackAddress() // 127.開頭的都是lookback地址
+								&& ip.getHostAddress().indexOf(":") == -1) {
+							bFindIP = true;
+
+							break;
+
+						}
+
+					}
+
+				}
+
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+		if (null != ip) {
+
+			sIP = ip.getHostAddress();
+
+		}
+
+		return sIP;
+
+	}
+
+}
---
+++ flowtable4j-core/src/main/java/com/ctrip/flowtable4j/core/Utils/JsonMapper.java
@@ -0,0 +1,93 @@
+package com.ctrip.flowtable4j.core.Utils;
+
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * 
+ * @author weiyu
+ * @date 2015年3月12日
+ */
+public class JsonMapper {
+	private static Logger logger = LoggerFactory.getLogger(JsonMapper.class);
+
+	private ObjectMapper mapper;
+
+	public JsonMapper() {
+		this(null);
+	}
+
+	public JsonMapper(Include include) {
+		mapper = new ObjectMapper();
+		//mapper.configure(DeserializationFeature., arg1)
+		// 设置输出时包含属性的风格
+		if (include != null) {
+			mapper.setSerializationInclusion(include);
+		}
+		// 设置输入时忽略在JSON字符串中存在但Java对象实际没有的属性
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+	}
+
+	/**
+	 * 创建只输出非Null且非Empty(如List.isEmpty)的属性到Json字符串的Mapper,建议在外部接口中使用.
+	 */
+	public static JsonMapper nonEmptyMapper() {
+		return new JsonMapper(Include.NON_EMPTY);
+	}
+
+	/**
+	 * 创建只输出初始值被改变的属性到Json字符串的Mapper, 最节约的存储方式，建议在内部接口中使用。
+	 */
+	public static JsonMapper nonDefaultMapper() {
+		return new JsonMapper(Include.NON_DEFAULT);
+	}
+
+	/**
+	 * Object可以是POJO，也可以是Collection或数组。 如果对象为Null, 返回"null". 如果集合为空集合, 返回"[]".
+	 */
+	public String toJson(Object object) {
+
+		try {
+			return mapper.writeValueAsString(object);
+		} catch (IOException e) {
+			logger.warn("write to json string error:" + object, e);
+			return null;
+		}
+	}
+
+	/**
+	 * 反序列化POJO或简单Collection如List<String>.
+	 * 
+	 * 如果JSON字符串为Null或"null"字符串, 返回Null. 如果JSON字符串为"[]", 返回空集合.
+	 * 
+	 * 如需反序列化复杂Collection如List<MyBean>, 请使用fromJson(String, JavaType)
+	 * 
+	 * @see #fromJson(String, JavaType)
+	 */
+	public <T> T fromJson(String jsonString, Class<T> clazz) {
+		if (jsonString == null || jsonString.trim().length() == 0) {
+			return null;
+		}
+
+		try {
+			return mapper.readValue(jsonString, clazz);
+		} catch (IOException e) {
+			logger.warn("parse json string error:" + jsonString, e);
+			return null;
+		}
+	}
+
+	/**
+	 * 取出Mapper做进一步的设置或使用其他序列化API.
+	 */
+	public ObjectMapper getMapper() {
+		return mapper;
+	}
+}
