3788e51204a35dd26ac853a31caffda9e8893264
unknown <lpxie@DST58695.cn1.global.ctrip.com>
2015-05-21 09:42:45 +0800

    addd logger

--- flowtable4j-translate/src/main/java/com/ctrip/infosec/flowtable4j/translate/service/HotelGroupExecutor.java
+++ flowtable4j-translate/src/main/java/com/ctrip/infosec/flowtable4j/translate/service/HotelGroupExecutor.java
@@ -137,7 +137,28 @@ public class HotelGroupExecutor implements Executor
 
             //预处理数据写到数据库
             flowData.put(Common.OrderType,data.get(Common.OrderType));
-            //writeDB(data,dataFact, flowData);//fixme 第一次测试先不写数据库
+            logger.info("dealInfo\t"+ Json.toPrettyJSONString(dataFact.dealInfo));
+            logger.info("mainInfo\t"+ Json.toPrettyJSONString(dataFact.mainInfo));
+            logger.info("contactInfo\t"+ Json.toPrettyJSONString(dataFact.contactInfo));
+            logger.info("userInfo\t"+ Json.toPrettyJSONString(dataFact.userInfo));
+            logger.info("ipInfo\t"+ Json.toPrettyJSONString(dataFact.ipInfo));
+            logger.info("hotelGroupInfo\t"+ Json.toPrettyJSONString(dataFact.productInfoM));
+            logger.info("otherInfo\t"+ Json.toPrettyJSONString(dataFact.otherInfo));
+            logger.info("DIDInfo\t"+ Json.toPrettyJSONString(dataFact.DIDInfo));
+            for(int i=0;i<dataFact.paymentInfoList.size();i++)
+            {
+                Map<String,Object> paymentInfo = dataFact.paymentInfoList.get(i);
+                logger.info(i + "\tpaymentInfo\t" + Json.toPrettyJSONString(paymentInfo));
+                List<Map<String,Object>> cardInfos = (List<Map<String,Object>>)paymentInfo.get(Common.CardInfoList);
+                for(int j=0;j<cardInfos.size();j++)
+                {
+                    logger.info(i + "\t" + j + "\tcardInfo\t" + Json.toPrettyJSONString(cardInfos.get(j)));
+                }
+            }
+            logger.info("paymentMainInfo\t"+ Json.toPrettyJSONString(dataFact.paymentMainInfo));
+
+            logger.info("流量表数据\t"+ Json.toPrettyJSONString(flowData));
+//            writeDB(data,dataFact, flowData);//fixme 第一次测试先不写数据库
 
         }catch (Exception exp)
         {
@@ -218,28 +239,6 @@ public class HotelGroupExecutor implements Executor
         //下面输出当前订单的预处理数据到日志 给测试用 方便他们做对比
         final String reqId = getValue(data,Common.ReqID);
         flowData.put(Common.ReqID,reqId);
-        logger.info("dealInfo\t"+ Json.toPrettyJSONString(dataFact.dealInfo));
-        logger.info("mainInfo\t"+ Json.toPrettyJSONString(dataFact.mainInfo));
-        logger.info("contactInfo\t"+ Json.toPrettyJSONString(dataFact.contactInfo));
-        logger.info("userInfo\t"+ Json.toPrettyJSONString(dataFact.userInfo));
-        logger.info("ipInfo\t"+ Json.toPrettyJSONString(dataFact.ipInfo));
-        logger.info("hotelGroupInfo\t"+ Json.toPrettyJSONString(dataFact.productInfoM));
-        logger.info("otherInfo\t"+ Json.toPrettyJSONString(dataFact.otherInfo));
-        logger.info("DIDInfo\t"+ Json.toPrettyJSONString(dataFact.DIDInfo));
-        for(int i=0;i<dataFact.paymentInfoList.size();i++)
-        {
-            Map<String,Object> paymentInfo = dataFact.paymentInfoList.get(i);
-            logger.info(i + "\tpaymentInfo\t" + Json.toPrettyJSONString(paymentInfo));
-            List<Map<String,Object>> cardInfos = (List<Map<String,Object>>)paymentInfo.get(Common.CardInfoList);
-            for(int j=0;j<cardInfos.size();j++)
-            {
-                logger.info(i + "\t" + j + "\tcardInfo\t" + Json.toPrettyJSONString(cardInfos.get(j)));
-            }
-        }
-        logger.info("paymentMainInfo\t"+ Json.toPrettyJSONString(dataFact.paymentMainInfo));
-
-        logger.info("流量表数据\t"+ Json.toPrettyJSONString(flowData));
-
 
         final DataFact dataFactCopy = BeanMapper.copy(dataFact,DataFact.class);
         List<Callable<DataFact>> runs = Lists.newArrayList();
